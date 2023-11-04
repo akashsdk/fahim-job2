55cd9bc69152df652e5a313d905504ddac66620a
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-04 23:09:16 +0600

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,50 @@
-import React from 'react'
-import './Home.css';
+import React, { Component } from 'react';
 
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [], // Array to store selected images
+    };
+  }
+
+  handleImageSelect = (event) => {
+    const fileInput = event.target;
+    const files = fileInput.files;
+
+    // Convert the FileList into an array and update the state
+    this.setState({
+      images: [...this.state.images, ...Array.from(files)],
+    });
+  };
+
+  handleImageDelete = (index) => {
+    const updatedImages = [...this.state.images];
+    updatedImages.splice(index, 1);
+
+    this.setState({
+      images: updatedImages,
+    });
+  };
+
+  render() {
+    const { images } = this.state;
+
+    return (
+      <div>
+        <input type="file" multiple onChange={this.handleImageSelect} />
+        <div>
+          {images.map((image, index) => (
+            <div key={index}>
+              <img src={URL.createObjectURL(image)} alt={`Image ${index + 1}`} />
+              <button onClick={() => this.handleImageDelete(index)}>Delete</button>
+            </div>
+          ))}
+        </div>
+        <p>Selected Images: {images.length}</p>
+      </div>
+    );
+  }
 }
+
+export default Home;
