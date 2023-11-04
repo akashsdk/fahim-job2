fbfec1018882b8c90697af1ba8938a36e8725786
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-04 23:43:38 +0600

    ok

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { FaTrash } from "react-icons/fa";
+import "./Home.css";
+
+const items = ["Image1.jpg", "Image2.jpg", "Image3.jpg", "Image4.jpg"];
 
 class Home extends Component {
-  constructor() {
-    super();
-    this.state = {
-      images: [], // Array to store selected images
-    };
-  }
+  state = {
+    images: items,
+    selectedImages: [],
+  };
+
+  onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const images = [...this.state.images];
+    const [reorderedItem] = images.splice(result.source.index, 1);
+    images.splice(result.destination.index, 0, reorderedItem);
+
+    this.setState({ images });
+  };
+
+  toggleImageSelection = (image) => {
+    const selectedImages = [...this.state.selectedImages];
+
+    if (selectedImages.includes(image)) {
+      selectedImages.splice(selectedImages.indexOf(image), 1);
+    } else {
+      selectedImages.push(image);
+    }
+
+    this.setState({ selectedImages });
+  };
+
+  deleteSelectedImages = () => {
+    const images = this.state.images.filter(
+      (image) => !this.state.selectedImages.includes(image)
+    );
+
+    this.setState({
+      images,
+      selectedImages: [],
+    });
+  };
 
   handleImageSelect = (event) => {
     const fileInput = event.target;
@@ -18,30 +56,55 @@ class Home extends Component {
     });
   };
 
-  handleImageDelete = (index) => {
-    const updatedImages = [...this.state.images];
-    updatedImages.splice(index, 1);
-
-    this.setState({
-      images: updatedImages,
-    });
-  };
-
   render() {
-    const { images } = this.state;
-
+    const { selectedImages } = this.state;
     return (
       <div>
+        <button onClick={this.deleteSelectedImages}>
+          <FaTrash /> Delete Selected
+        </button>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="imageGallery" direction="horizontal">
+            {(provided) => (
+              <div
+                className="image-gallery"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {this.state.images.map((image, index) => (
+                  <Draggable key={image} draggableId={image} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        className={`image-item ${
+                          snapshot.isDragging ? "dragging" : ""
+                        }`}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <div
+                          className={`image ${
+                            this.state.selectedImages.includes(image)
+                              ? "selected"
+                              : ""
+                          }`}
+                          onClick={() => this.toggleImageSelection(image)}
+                        >
+                          <img src={image} alt={image} />
+                          
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+
         <input type="file" multiple onChange={this.handleImageSelect} />
-        <div>
-          {images.map((image, index) => (
-            <div key={index}>
-              <img src={URL.createObjectURL(image)} alt={`Image ${index + 1}`} />
-              <button onClick={() => this.handleImageDelete(index)}>Delete</button>
-            </div>
-          ))}
-        </div>
-        <p>Selected Images: {images.length}</p>
+        <p>Selected Images: {selectedImages.length}</p>
       </div>
     );
   }
